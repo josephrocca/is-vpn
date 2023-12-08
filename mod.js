3c5698067e7650f3e9d77b05804e501ca4383af5
josephrocca <1167575+josephrocca@users.noreply.github.com>
2023-12-08 19:12:35 +0800

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,6 +1,12 @@
-const ipv4CidrRanges = await fetch("https://raw.githubusercontent.com/josephrocca/is-vpn/main/vpn-or-datacenter-ipv4-ranges.txt").then(r => r.text()).then(t => t.trim().split("\n"));
+let ipv4BinaryRanges;
 
-const ipv4BinaryRanges = ipv4CidrRanges.map(ipv4CidrToRange);
+function updateList() {
+  const ipv4CidrRanges = await fetch("https://raw.githubusercontent.com/josephrocca/is-vpn/main/vpn-or-datacenter-ipv4-ranges.txt").then(r => r.text()).then(t => t.trim().split("\n"));
+  ipv4BinaryRanges = ipv4CidrRanges.map(ipv4CidrToRange);
+}
+
+await updateList();
+setInterval(updateList, 1000*60*60*12);
 
 function ipToBinary(ip) {
   return ip.split('.').reduce((acc, octet) => (acc << 8) + parseInt(octet, 10), 0);
