ed18945a04269c5cb80b019b199ccaca4dfc9329
josephrocca <1167575+josephrocca@users.noreply.github.com>
2023-12-08 19:08:51 +0800

    Create mod.js

---
+++ mod.js
@@ -0,0 +1,21 @@
+const ipv4CidrRanges = await fetch("https://raw.githubusercontent.com/josephrocca/is-vpn/main/vpn-or-datacenter-ipv4-ranges.txt").then(r => r.text()).then(t => t.trim().split("\n"));
+
+const ipv4BinaryRanges = ipv4CidrRanges.map(ipv4CidrToRange);
+
+function ipToBinary(ip) {
+  return ip.split('.').reduce((acc, octet) => (acc << 8) + parseInt(octet, 10), 0);
+}
+
+function ipv4CidrToRange(cidr) {
+  const [baseIp, subnetMask] = cidr.split('/');
+  const ipBinary = ipToBinary(baseIp);
+  const rangeStart = ipBinary;
+  const rangeEnd = ipBinary | ((1 << (32 - subnetMask)) - 1);
+  return [rangeStart, rangeEnd];
+}
+
+// TODO: improve performance with interval tree
+export function isVpn(ip) {
+  const ipBinary = ipToBinary(ip);
+  return ipv4BinaryRanges.some(([start, end]) => ipBinary >= start && ipBinary <= end);
+}
