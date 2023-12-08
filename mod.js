10d892c05b63eb4ce1905c0e312412fe67164702
josephrocca <1167575+josephrocca@users.noreply.github.com>
2023-12-08 20:00:34 +0800

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,12 +1,73 @@
-let ipv4BinaryRanges;
+// we use an interval tree to quickly query a large number of CIDR IP ranges.
+// the naive approach takes ~7ms per query on my laptop, whereas this takes 0.2ms.
+
+class IntervalNode {
+  constructor(interval) {
+    this.interval = interval; // interval is an object like {start: x, end: y}
+    this.max = interval.end;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class IntervalTree {
+  constructor() {
+    this.root = null;
+  }
+  
+  insert(interval) {
+    if (!this.root) {
+      this.root = new IntervalNode(interval);
+      return;
+    }
+
+    let node = this.root;
+    while (true) {
+      node.max = Math.max(node.max, interval.end);
+
+      if (interval.start < node.interval.start) {
+        if (!node.left) {
+          node.left = new IntervalNode(interval);
+          break;
+        }
+        node = node.left;
+      } else {
+        if (!node.right) {
+          node.right = new IntervalNode(interval);
+          break;
+        }
+        node = node.right;
+      }
+    }
+  }
+
+  query(point) {
+    let node = this.root;
+    while (node) {
+      if (point >= node.interval.start && point <= node.interval.end) {
+        return true;
+      }
+
+      if (node.left && point <= node.left.max) {
+        node = node.left;
+      } else {
+        node = node.right;
+      }
+    }
+    return false;
+  }
+}
+
+let itree;
 
 async function updateList() {
   const ipv4CidrRanges = await fetch("https://raw.githubusercontent.com/josephrocca/is-vpn/main/vpn-or-datacenter-ipv4-ranges.txt").then(r => r.text()).then(t => t.trim().split("\n"));
-  ipv4BinaryRanges = ipv4CidrRanges.map(ipv4CidrToRange);
+  itree = new IntervalTree();
+  ipv4CidrRanges.map(ipv4CidrToRange).forEach(range => itree.insert(range));
 }
 
 await updateList();
-setInterval(updateList, 1000*60*60*12);
+setInterval(updateList, 1000*60*60*12); // fetch updated ip range list every 12 hours
 
 function ipToBinary(ip) {
   return ip.split('.').reduce((acc, octet) => (acc << 8) + parseInt(octet, 10), 0);
@@ -17,11 +78,10 @@ function ipv4CidrToRange(cidr) {
   const ipBinary = ipToBinary(baseIp);
   const rangeStart = ipBinary;
   const rangeEnd = ipBinary | ((1 << (32 - subnetMask)) - 1);
-  return [rangeStart, rangeEnd];
+  return {start:rangeStart, end:rangeEnd};
 }
 
-// TODO: improve performance with interval tree
 export function isVpn(ip) {
   const ipBinary = ipToBinary(ip);
-  return ipv4BinaryRanges.some(([start, end]) => ipBinary >= start && ipBinary <= end);
+  return itree.query(ipBinary);
 }
